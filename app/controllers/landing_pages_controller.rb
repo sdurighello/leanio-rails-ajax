0bb51bd64c8e9ebf6b9c4c82f27692c2eeb6454f
caisepolente <steve.durighello@gmail.com>
2016-09-15 23:47:27 +0200

    disable breadcrumb on landign page

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,15 @@
 class LandingPagesController < ApplicationController
+
+  before_filter :disable_breadcrumb
+
   def index
+
   end
+
+  private
+
+  def disable_breadcrumb
+    @disabled_breadcrumb = true
+  end
+
 end
