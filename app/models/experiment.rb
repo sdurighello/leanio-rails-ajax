85b81d0ffe624e405ddc48ad51850b0086924025
caisepolente <steve.durighello@gmail.com>
2016-09-15 22:01:56 +0200

    multi-part form flow completed

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -2,25 +2,25 @@ class Experiment < ApplicationRecord
   belongs_to :phase
   has_and_belongs_to_many :sprints
 
-  validates :name, presence: true, if: -> { required_for_step?(:identity) }
+  validates :name, presence: true, if: -> { required_for_step?(:header) }
 
   # Multi step form wicked
 
   # class attribute accessor
   cattr_accessor :form_steps do
-    %w(identity characteristics instructions)
+    %w(header description result)
   end
 
   # instance level accessor to know in which step are we
   attr_accessor :form_step
 
-  def required_for_step?(step)
+  def required_for_step?(form_step)
     # All fields are required if no form step is present
     return true if form_step.nil?
 
     # All fields from previous steps are required if the
     # step parameter appears before or we are on the current step
-    return true if self.form_steps.index(step.to_s) { self.form_steps.index(form_step) }
+    return true if self.form_steps.index(form_step.to_s) { self.form_steps.index(form_step) }
   end
 
 
