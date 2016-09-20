444f83118734fa10d6701ebab629cbc519ba3ff6
caisepolente <steve.durighello@gmail.com>
2016-09-20 07:01:44 +0200

    removed unnecessary fields in experiment

--- db/migrate/20160914071628_create_experiments.rb
+++ db/migrate/20160914071628_create_experiments.rb
@@ -2,17 +2,8 @@ class CreateExperiments < ActiveRecord::Migration[5.0]
   def change
     create_table :experiments do |t|
       t.string :name
-      t.date :start_date
-      t.date :end_date
+      t.text :description
       t.boolean :completed
-      t.string :domain
-      t.text :assumption
-      t.text :method
-      t.text :observation
-      t.text :measure
-      t.text :learned
-      t.text :success_criteria
-      t.text :action
       t.integer :interviews_planned
       t.integer :interviews_done
       t.integer :early_adopters_planned
