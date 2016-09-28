d7541ff4cbbd677aca0576032c3a6ffbb6d330c6
caisepolente <steve.durighello@gmail.com>
2016-09-28 12:26:37 +0200

    redone show canvas and filter

--- app/models/area.rb
+++ app/models/area.rb
@@ -34,4 +34,11 @@ class Area < ApplicationRecord
     end
   end
 
+  def options_for_select_hypotheses
+    # Get only hypotheses with that area_identifier that have not yet been associated with this area
+    h_not_associated = Hypothesis.includes(:areas).where(area_identifier: self.area_identifier).where(areas: {id: nil})
+    h_not_this_area = Hypothesis.includes(:areas).where(area_identifier: self.area_identifier).where.not(areas: {id: self.id})
+    (h_not_associated + h_not_this_area).map { |h| [h.name, h.id] }
+  end
+
 end
