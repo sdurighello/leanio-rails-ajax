13c049cb54ba184b42347884f30ff9262397331a
caisepolente <steve.durighello@gmail.com>
2016-09-29 10:57:03 +0200

    reordered application css and js entries

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
-
-//= require bootstrap-sprockets
-
-//= require jquery-ui
