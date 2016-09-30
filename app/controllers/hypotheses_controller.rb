789706d87ea9b0eef41e5c2013808db3afe59287
caisepolente <steve.durighello@gmail.com>
2016-09-30 08:28:48 +0200

    ajax modal hypothesis

--- app/controllers/hypotheses_controller.rb
+++ app/controllers/hypotheses_controller.rb
@@ -59,7 +59,7 @@ class HypothesesController < ApplicationController
     respond_to do |format|
       if @hypothesis.update(hypothesis_params)
         format.html { redirect_to project_hypothesis_path(@project.id, @hypothesis), notice: 'Hypothesis was successfully updated.' }
-        format.json { @hypothesis }
+        format.json { render json: @hypothesis, status: :ok }
       else
         format.html { render :edit }
         format.json { render json: @hypothesis.errors, status: :unprocessable_entity }
